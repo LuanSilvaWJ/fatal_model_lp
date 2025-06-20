c12db9236ed3b43f5458893c051e389720f696a7
Luan Henrique <luansilva@wjcasino.bet.br>
2025-06-20 14:52:10 -0300

    change on 'redeem code' link to mobile link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function CasinoLanding() {
     setShowPopup(true);
     navigator.clipboard.writeText(code);
     setTimeout(() => {
-      window.location.href = "https://wjcasino.bet.br/?aptwjcasino";
+      window.location.href = "https://wjcasino.bet.br/register";
     }, 5000);
   }
 
